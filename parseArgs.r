586f80c138f275dfcad7b9abe064cce96f38045d
Leon <leon@Leons-MacBook-Pro.local>
2014-07-10 09:50:19 +0800

    fix incorrectly parsing class weight arguments

--- parseArgs.r
+++ parseArgs.r
@@ -1,22 +1,28 @@
 parseArgs = function(...){
-  cArguments = function(arg, subj){
-    if(inherits(subj,c('character'))){
-      return(sprintf('--%s="%s"',arg, subj))
-    } else if(inherits(subj,c('numeric','integer'))){
-      return(sprintf('--%s=%s',arg,subj))
-    } else if(inherits(subj,'logical')) {
-        #print(arg)
-        #print(subj)
-        if(subj)return(sprintf('--%s',arg))
-    } else return('')
-  }
-  args = list(...)
-  out = list()
-  if('' %in% names(args)){
-    stop('parseArgs accepts only keyword arguments')
-  }
-  for(i in names(args)){
-    out[[i]] = cArguments(i,args[[i]])
-  }
-  return(out)
+    cArguments = function(arg, subj){
+        if(inherits(subj,c('character'))){
+            return(sprintf('--%s="%s"',arg, subj))
+        } else if(inherits(subj,c('numeric','integer'))){
+            if(length(subj)>1) {
+                subj = sprintf("%s;",
+                               do.call(paste,
+                                lapply(c(subj,sep=";"),
+									function(x){return(x)})))
+            }
+            return(sprintf('--%s=%s',arg,subj))
+        } else if(inherits(subj,'logical')) {
+            #print(arg)
+            #print(subj)
+            if(subj)return(sprintf('--%s',arg))
+        } else return('')
+    }
+    args = list(...)
+    out = list()
+    if('' %in% names(args)){
+        stop('parseArgs accepts only keyword arguments')
+    }
+    for(i in names(args)){
+        out[[i]] = cArguments(i,args[[i]])
+    }
+    return(out)
 }
